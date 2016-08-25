ad18ce3ac1dc3a3f52475b0e0739055376c4e9bf
Mirko Geissler <mirko.geissler@artcom.de>
2016-08-25 10:47:51 +0200

    Refactor methods

--- main.js
+++ main.js
@@ -7,15 +7,18 @@ export default async function Init(serviceId, bootstrapUrl) {
 
   log.info({ bootstrapUrl }, "Retrieving bootstrap data")
   const data = await queryBootstrapData(bootstrapUrl)
+
   log.info({ data }, "Bootstrap data retrieved")
 
-  const mqttClient = connectToServer(
-    serviceId,
-    log,
-    data.device,
-    data.tcpBrokerUri,
-    data.httpBrokerUri
-  )
+  const { device, tcpBrokerUri, httpBrokerUri } = data
+
+  const clientId = createClientId(serviceId, device)
+  log.info({ tcpBrokerUri, httpBrokerUri, clientId }, "Connecting to broker")
+  const mqttClient = topping.connect(tcpBrokerUri, httpBrokerUri, clientId)
+
+  mqttClient.on("connect", () => { log.info("Connected to Broker") })
+  mqttClient.on("close", () => { log.error("Disconnected from Broker") })
+  mqttClient.on("error", () => { log.error("Error Connecting to Broker") })
 
   return { log, mqttClient, ...data }
 }
@@ -40,16 +43,6 @@ function delay(time) {
   })
 }
 
-function connectToServer(serviceId, log, device, tcpBrokerUri, httpBrokerUri) {
-  const mqttClient = topping.connect(
-    tcpBrokerUri,
-    httpBrokerUri,
-    `${serviceId}-${device}-${Math.random().toString(16).substr(2, 8)}`
-  )
-
-  mqttClient.on("connect", () => { log.info("Connected to Broker") })
-  mqttClient.on("close", () => { log.error("Disconnected from Broker") })
-  mqttClient.on("error", () => { log.error("Error Connecting to Broker") })
-
-  return mqttClient
+function createClientId(serviceId, device) {
+  return `${serviceId}-${device}-${Math.random().toString(16).substr(2, 8)}`
 }
